024e1bbac99eb63bfdd115365f4d2212645654e7
Jake Gwynn <74085669+JakeGwynn@users.noreply.github.com>
2022-09-07 08:33:00 -0700

    Update Remove-JunkEmailBlockedSenders.ps1

--- Remove-JunkEmailBlockedSenders.ps1
+++ Remove-JunkEmailBlockedSenders.ps1
@@ -2,23 +2,25 @@ Connect-ExchangeOnline
 $AdminUpn = "jakegwynn@jakegwynndemo.com"
 $Mailboxes = Get-EXOMailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox
 $EmailOrDomainToRemove = "block2@gmail.com"
+$MoreThan10000Users = $false
 
-foreach ($input in $Mailboxes) {
-    $input.Name 
-    $JunkConfig = $null
-    $JunkConfig = Get-MailboxJunkEmailConfiguration $input.Name
-    if ($JunkConfig.BlockedSendersAndDomains -contains "$EmailOrDomainToRemove") {
-        Set-MailboxJunkEmailConfiguration $input.Name -BlockedSendersAndDomains @{Remove="$EmailOrDomainToRemove"}
+if ($MoreThan10000Users -eq $false) {
+    foreach ($input in $Mailboxes) {
+        $input.Name 
+        $JunkConfig = $null
+        $JunkConfig = Get-MailboxJunkEmailConfiguration $input.Name
+        if ($JunkConfig.BlockedSendersAndDomains -contains "$EmailOrDomainToRemove") {
+            Set-MailboxJunkEmailConfiguration $input.Name -BlockedSendersAndDomains @{Remove="$EmailOrDomainToRemove"}
+        }
     }
 }
-
-<#
-Start-RobustCloudCommand -UserPrincipalName $AdminUpn -Recipients $Mailboxes -LogFile C:\temp\out.log -ScriptBlock {
-    $JunkConfig = $null
-    $JunkConfig = Get-MailboxJunkEmailConfiguration $input.Name
-    if ($JunkConfig.BlockedSendersAndDomains -contains "$EmailOrDomainToRemove") {
-        Write-Host "Removing $EmailOrDomainToRemove from $($input.DisplayName)"
-        Set-MailboxJunkEmailConfiguration $input.Name -BlockedSendersAndDomains @{Remove="$EmailOrDomainToRemove"}
+else {
+    Start-RobustCloudCommand -UserPrincipalName $AdminUpn -Recipients $Mailboxes -LogFile C:\temp\out.log -ScriptBlock {
+        $JunkConfig = $null
+        $JunkConfig = Get-MailboxJunkEmailConfiguration $input.Name
+        if ($JunkConfig.BlockedSendersAndDomains -contains "$EmailOrDomainToRemove") {
+            Write-Host "Removing $EmailOrDomainToRemove from $($input.DisplayName)"
+            Set-MailboxJunkEmailConfiguration $input.Name -BlockedSendersAndDomains @{Remove="$EmailOrDomainToRemove"}
+        }
     }
 }
-#>
